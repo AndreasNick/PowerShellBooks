7670073dbac4ae03b3ab7554b981c164fd5afea8
Andreas Nick <a.nick@nick-it.de>
2019-07-17 10:52:34 +0200

    develop

--- test-PowerShellBooks.ps1
+++ test-PowerShellBooks.ps1
@@ -2,20 +2,18 @@ Import-Module $PSScriptRoot\PowerShellBooks -Force
 
 
 
-$Module = 'DnsClient'
+$Command = 'DISM'
 
 $OutFile = '{0}\desktop\Powershell_With_{1}.pdf' -f $env:userprofile, $Command
 
-
-#New-CommandDocumentation -OutputPdfDocument $OutFile -Command $Command 
-
-New-PowerShellBook -OutputPdfDocument $OutFile -Module $Module
+New-CommandDocumentation -OutputPdfDocument $OutFile -Command $Command 
 
 
 
 #$pdf.AddHeader((New-Object iTextSharp.text.Paragraph "Header"))
 <#
-  #Add-Image -Document $pdf -File "$PSScriptRoot\kitten1.jpg"
+
+    #Add-Image -Document $pdf -File "$PSScriptRoot\kitten1.jpg"
     Add-Table -Document $pdf -Dataset @('Name', 'Mittens', 'Age', '1.5', 'Fur color', 'Black and white', 'Favorite toy', 'String') -Cols 2 -Centered
     #Add-Image -Document $pdf -File "$PSScriptRoot\kitten2.jpg"
     Add-Table -Document $pdf -Dataset @('Name', 'Achilles', 'Age', '2', 'Fur color', 'Grey', 'Favorite toy', 'Shoes') -Cols 2 -Centered
